3f559a9b5ed030438bbc343cf603a38d8bd592b6
George Michopoulos <giorgos@stanford.edu>
2013-05-01 12:58:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperchain::Application.config.secret_token = 'd0b6a9eb89397105b5c4486431e1c3201938f24ca19131835e1a9fdb2fc87dff90d4d640c772a9d8ede15efc1f78eb978222e9b4b8cbd65c2df966754897df02'
