5189a083672d90bb06bafd85978f1241d9b8d9d3
George Michopoulos <george.michopoulos@gmail.com>
2013-05-13 16:16:34 -0700

    changed application.rb to allow heroku to precompile

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+
 end
 
 module Paperchain
@@ -58,5 +59,8 @@ module Paperchain
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent db from connecting on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
