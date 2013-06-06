64262d628ed5c3e46fa6c872e0bde791c452f9c5
George Michopoulos <george.michopoulos@gmail.com>
2013-06-06 13:30:20 -0700

    enabled login/logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_login_debug
+  # before_filter :check_login_debug
   before_filter :check_login
 
-  def check_login_debug
-    session[:curr_author] = 3
-  end
+  # def check_login_debug
+  #   session[:curr_author] = 3
+  # end
 
   def check_login
     if session[:curr_author]
