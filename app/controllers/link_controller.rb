b88c819e28f3df3bf9d1407aadfabb3f69df205b
Alex Cope <alexjcope@gmail.com>
2013-06-06 02:15:26 -0700

    More work done whatever

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -3,6 +3,14 @@ class LinkController < ApplicationController
 def index
   @link = Link.find(params[:id])
   @wordcount = @link.link_text.split.length
+  @prev = @next = nil
+  links = @link.chain.links
+  links.length.times do |i|
+    if links[i] == @link
+      @prev = links[i-1] unless i == 0
+      @next = links[i+1] unless i == links.length-1
+    end
+  end
 end
 
 end
