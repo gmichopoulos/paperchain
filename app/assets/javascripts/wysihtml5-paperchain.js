5e7f5b78549f9f7a9ab8659955841df8750258b9
Alex Cope <alexjcope@gmail.com>
2013-06-07 02:58:02 -0700

    Text editor is beautiful. Also other stuff.

--- app/assets/javascripts/wysihtml5-paperchain.js
+++ app/assets/javascripts/wysihtml5-paperchain.js
@@ -21,15 +21,115 @@ Wysihtml5.prototype = {
 
   createEditor: function() {
     var editor = new wysihtml5.Editor(this.el[0], {
-      toolbar: "wysihtml5-toolbar",
+      toolbar: null,
       parserRules: wysihtml5ParserRules,
       stylesheets: ["/assets/editor.css"],
     });
+    return editor;
   },
 
   initSelection: function() {
-    this.parentEl.mouseup( function() {
-      console.log('select!');
+    this.editor.on('load', function() {
+      var editor = this;
+
+      var showTooltip = function(evt) {
+        var selection = editor.composer.selection.getSelection();
+
+        if ($('.tooltip').length > 0) {
+          $('.tooltip').hide('slow').remove();
+          $('.tooltip-triangle').hide('slow').remove();
+        }
+
+        if (selection.toHtml().length > 0) {
+
+          var rect = selection.getBoundingDocumentRect();
+
+          var tooltip = $("<div>");
+          tooltip.attr('class', 'tooltip');
+
+          var sep = $("<div>");
+          sep.attr('class', 'tooltip-sep');
+
+          var bold = $("<div>");
+          bold.attr('class', 'tooltip-bold');
+
+          var ital = $("<div>");
+          ital.attr('class', 'tooltip-italic');
+
+          var und = $("<div>");
+          und.attr('class', 'tooltip-underline');
+
+          var sml = $("<div>");
+          sml.attr('class', 'tooltip-smaller');
+
+          var big = $("<div>");
+          big.attr('class', 'tooltip-bigger');
+
+          // tooltip.click( function() {
+          //   console.log('yo');
+          //   console.log(editor.composer.selection.getSelection());
+          //   editor.composer.commands.exec("bold");
+          // })
+
+          bold.click( function() {
+            editor.composer.commands.exec("bold");
+          })
+
+          ital.click( function() {
+            editor.composer.commands.exec("italic");
+          })
+
+          und.click( function() {
+            editor.composer.commands.exec("underline");
+          })
+
+          // TODO: implement font size bigger/smaller
+
+          tooltip.append(bold);
+          tooltip.append(sep.clone());
+          tooltip.append(ital);
+          tooltip.append(sep.clone());
+          tooltip.append(und);
+
+          //super fearing
+          var num_formats = Math.ceil(tooltip.children().length/2);
+          var width = num_formats*(12+8*2) + (num_formats-1);
+          var adjust = Math.floor(width/2);
+
+          tooltip.css({
+            left: (rect.left+Math.floor(rect.width/2)-adjust) + "px",
+            top: (rect.top-40) + "px",
+          });
+
+          //triangle
+          var triangle = $("<div>");
+          triangle.attr('class', 'tooltip-triangle');
+          triangle.css({
+            left: (rect.left+Math.floor(rect.width/2)) + "px",
+            top: (rect.top-5) + "px",
+          });
+
+
+          $('#text').append(tooltip);
+          tooltip.hide();
+          tooltip.fadeIn(200);
+          $('#text').append(triangle);
+          triangle.hide();
+          triangle.fadeIn(200);
+
+        }
+      };
+
+      var updateWordCount = function() {
+        var text = $('#text').wysihtml5Text();
+        var count = text.split(' ').length;
+        $('.wordcount').html(count + ' words');
+      }
+
+      editor.composer.element.addEventListener('mouseup', showTooltip);
+      editor.composer.element.addEventListener('keyup', showTooltip);
+      editor.composer.element.addEventListener('keyup', updateWordCount);
+
     });
   }
 
